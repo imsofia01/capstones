b0307e88387ea941a0e720fcac1e1102a19072c5
imsofia01 <bocaligsofia22@gmail.com>
2023-09-11 15:23:12 +0800

    remove loader

--- loading.php
+++ loading.php
@@ -11,12 +11,9 @@
 
 <h2>TUKLASKO</h2>
 
-
+<a href="home.php">
 <button id="startButton"> MAGSIMULA </button>
-    <div class="loader-container">
-        <div class="loader" id="loader"></div>
-    </div>
-</div>
+</a>
         
 </body>
 <script>
@@ -35,34 +32,6 @@ setInterval(() => {
   document.body.style.setProperty('--background', color)
 }, 5000)
 
-const startButton = document.getElementById('startButton');
-const loaderContainer = document.querySelector('.loader-container');
-const loader = document.getElementById('loader');
-
-startButton.addEventListener('click', () => {
-    // Display the loading bar
-    loaderContainer.style.display = 'block';
-
-    // Simulate loading (set timeout for demonstration purposes)
-    simulateLoading();
-});
-
-function simulateLoading() {
-    let progress = 0;
-    const interval = setInterval(() => {
-        progress += 1;
-        loader.style.borderTop = `16px solid #3498db`;
-        loader.style.borderRight = `16px solid transparent`;
-        loader.style.transform = `translate(-50%, -50%) rotate(${progress * 3.6}deg)`;
-
-        if (progress >= 100) {
-            clearInterval(interval);
-
-            // Redirect to the next page after loading is complete
-            window.location.href = 'home.php';
-        }
-    }, 50);
-}
  
 </script>
 </html>
