f3eb5398fa52853ed36824d1f1e14c3706286cab
imsofia01 <131735572+imsofia01@users.noreply.github.com>
2023-10-21 13:55:57 +0000

    asking message after video

--- video2.php
+++ video2.php
@@ -77,10 +77,13 @@ arrow_back
     }
 
     function goToNextPage() {
-        // Redirect to the next page when the video ends
-        window.location.href = 'save-namesD.php'; // Replace with the actual URL of the next page
-    }
-});
+            // Redirect to the next page when the video ends
+            var result = confirm("Gusto mo bang ipag-patuloy ang pagsusulit?");
+            if (result) {
+                window.location.href = 'save-names.php'; // Replace with the actual URL of the assessment page
+            }
+        }
+    });
 
     </script>
 
